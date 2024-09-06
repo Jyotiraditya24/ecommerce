e4165b6b5a795ee0484905f89ccf1b3a98bfae2a
jyotiraditya <jyotiraditya24@gmail.com>
2024-09-06 15:00:19 +0530

    Admin Route and protected route done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,12 @@
-export const getAllProducts = (req,resp)=>{
-    
-}
+import Product from "../models/Product.model";
+
+export const getAllProducts = async (req, resp) => {
+  try {
+    const products = await Product.find();
+    return resp.json({ products });
+  } catch (error) {
+    resp
+      .status(500)
+      .json({ message: "Error in getAllProducts route", error: error.message });
+  }
+};
---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+export const protectedRoute = async (req, resp, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) {
+      return resp.status(401).json({ message: "Unauthorized" });
+    }
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findOne({ userId: decoded.userId }).select(
+        "-password"
+      );
+      if (!user) {
+        return resp.status(401).json({ message: "User not found " });
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return resp.status(401).json({ message: "Token Expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    console.log("Error in protected route middleware", error.message);
+    resp.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+export const adminRoute = async (req, resp, next) => {
+  try {
+    const user = req.user;
+    if (user && user.role === "admin") {
+      next();
+    } else {
+      return resp.status(401).json({ message: "Unauthorized Admin only" });
+    }
+  } catch (error) {
+    console.log("Error in admin Route", error.message);
+    resp.status(401).json({ message: "Unauthorized Admin only" });
+  }
+};
