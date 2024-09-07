c031ed5d23610bf56190b2ca73e3e92e5cd7df3b
jyotiraditya <jyotiraditya24@gmail.com>
2024-09-07 23:40:17 +0530

    cloudinary step up 1.0

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { redis } from "../lib/redis.js";
 import Product from "../models/Product.model.js";
 
@@ -20,7 +21,7 @@ export const getFeaturedProducts = async (req, resp) => {
     }
     // if not in redis we will fetch it from mongo DB
     featuredProducts = await Product.find({ isFeatured: true }).lean(); // returns js objects instead of moongose objects making the performance faster
-    if (!featuredProducts) { 
+    if (!featuredProducts) {
       resp.status(404).json({ message: "No featured products found" });
     }
     // store in cache for quick access
@@ -31,4 +32,48 @@ export const getFeaturedProducts = async (req, resp) => {
     console.log("Error in getFeaturedProducts", error.message);
     resp.status(500).json({ message: "Server error", error: error.message });
   }
+};
+
+export const createProduct = async (req, resp) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+    let cloudinaryResponse = null;
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url,
+      category,
+    });
+
+    resp.status(201).json(product);
+  } catch (error) {
+    console.log("Error in createProduct", error.message);
+    resp.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const deleteProduct = (req, resp) => {
+  try {
+    const { id: productId } = req.params;
+    const product = Product.findById(productId);
+    if (!product) {
+      resp.status(404).json({ message: "Product not found" });
+    }
+    if (product.image) {
+      const publicId = product.image.split("/").pop().split(".")[0];
+      try {
+        cloudinary.uploader.destroy(`products/${publicId}`);
+        console.log("Image deleted");
+      } catch (error) {
+        console.log("Error deleting image from cloudinary");
+      }
+    }
+    
+  } catch (error) {}
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { getAllProducts,getFeaturedProducts } from "../controllers/product.controller.js";
-import { protectedRoute,adminRoute } from "../middleware/auth.middleware.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+  createProduct,
+  deleteProduct,
+} from "../controllers/product.controller.js";
+import { protectedRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectedRoute, adminRoute, getAllProducts);
-
-router.get("/",getFeaturedProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/", protectedRoute, adminRoute, createProduct);
+router.post("/:id", protectedRoute, adminRoute, deleteProduct);
 
 export default router;
