8580c924f62b0c8cd0da61f8f1bb11e63a8656e6
jyotiraditya <jyotiraditya24@gmail.com>
2024-09-17 17:49:56 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
@@ -19,6 +20,9 @@ app.use(cors({ origin: true, credentials: true }));
 
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+console.log(__dirname);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
@@ -29,6 +33,12 @@ app.use("/api/coupon", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, resp) => {
+    resp.sendFile(path, join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log("server is running on http://localhost:" + PORT);
   connectDb();
